3672bf0005a388b8ba1148a1bbcf3d5400c122d7
xinkai <1174307481@qq.com>
2025-07-05 21:41:14 +0800

    1

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs']
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client')
+    }
+    return config
+  }
+}
+
+module.exports = nextConfig
